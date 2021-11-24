dac8fb30bb585754a1914bcb8f90e1a8f440e90d
dennis-kya80 <94791709+dennis-kya80@users.noreply.github.com>
2021-11-24 09:48:27 +0000

    initial commit

---
+++ integers.py
@@ -0,0 +1,7 @@
+player=input("enter players award")
+if player==1:
+    print("gold")
+if player==2:
+    print("silver")
+if player==3:
+    print("bronze")
---
+++ wednesday.py
@@ -0,0 +1,9 @@
+player=input("enter players number")
+if player==1 and player==2:
+    print("gold")
+else:
+    print("silver")
+if player==2 and player==3:
+    print("silver")
+else:
+    print("bronze")
